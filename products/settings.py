54dade29210da0c713b8c0a2ae3f08d34e5397d1
Jawad Khan <Jawad.khan@arbisoft.com>
2015-09-07 16:02:22 +0500

    going to heroku

--- products/settings.py
+++ products/settings.py
@@ -77,17 +77,28 @@ WSGI_APPLICATION = 'products.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'product',
+#         'USER': 'root',
+#         'PASSWORD': '123',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'product',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mydb',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '123',
-        'HOST': '',
-        'PORT': '',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
