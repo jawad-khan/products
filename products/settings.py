6415a4ae527eb0f6b75add9194d9c976caa9b0a6
Jawad Khan <Jawad.khan@arbisoft.com>
2015-09-07 16:57:26 +0500

    goiing to heroku

--- products/settings.py
+++ products/settings.py
@@ -88,10 +88,11 @@ WSGI_APPLICATION = 'products.wsgi.application'
 #     }
 # }
 #
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mydb',                      # Or path to database file if using sqlite3.
+        'NAME': 'product',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '123',
@@ -121,3 +122,23 @@ STATIC_URL = '/static/'
 MEDIA_URL='/product/images/'
 LOGIN_URL='/app/login'
 LOGIN_REDIRECT_URL='/app/home'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
